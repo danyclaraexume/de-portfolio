5e91b4135fbd18c10aa42c3743356a2588937571
danyclaraexume <dany.exume@outlook.com>
2023-10-30 21:52:32 -0400

    Updates

---
+++ precache-manifest.b3c22021953f78142f5663d8ef2f0c9f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c83a1b913fc76cb8e8d9f71fde266c2",
+    "url": "/de-portfolio/index.html"
+  },
+  {
+    "revision": "b3761e1e7ec6adf79429",
+    "url": "/de-portfolio/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "1b788413324ee71b1df0",
+    "url": "/de-portfolio/static/css/main.6c182444.chunk.css"
+  },
+  {
+    "revision": "b3761e1e7ec6adf79429",
+    "url": "/de-portfolio/static/js/2.90ff8d63.chunk.js"
+  },
+  {
+    "revision": "435881bb28fec4d4e84baf9dfd194218",
+    "url": "/de-portfolio/static/js/2.90ff8d63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb6179ed507fafed2e6d",
+    "url": "/de-portfolio/static/js/3.752d46b2.chunk.js"
+  },
+  {
+    "revision": "1b788413324ee71b1df0",
+    "url": "/de-portfolio/static/js/main.a48305aa.chunk.js"
+  },
+  {
+    "revision": "9edd85784c786305a6b8",
+    "url": "/de-portfolio/static/js/runtime-main.b1405680.js"
+  },
+  {
+    "revision": "741a50c3c5712e5ce4eba7567e708608",
+    "url": "/de-portfolio/static/media/cv-icon.741a50c3.png"
+  },
+  {
+    "revision": "6e12f94f76390f11b25faa0ef6e57d00",
+    "url": "/de-portfolio/static/media/email-icon.6e12f94f.png"
+  },
+  {
+    "revision": "1b448f6ffe1d89a711efc1b7f6428186",
+    "url": "/de-portfolio/static/media/github-icon.1b448f6f.png"
+  },
+  {
+    "revision": "7c095d197c8f32be1345c165ede274e5",
+    "url": "/de-portfolio/static/media/profile-img.7c095d19.jpeg"
+  }
+]);
